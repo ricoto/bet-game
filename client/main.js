acff6c0d1a19e920888f9666f0cce63d744c0fff
Stanislav Palval <ricoto86@MacBook-Pro.local>
2020-12-28 23:34:51 -0500

    added ui

--- client/main.js
+++ client/main.js
@@ -1,12 +1,15 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
+var walletAddress;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts) {
-      contractInstance = new web3.eth.Contract(window.abi, "0xE17F1b29A707a546E97B46154a6807A196e54509", {from: accounts[0]});
+      walletAddress = accounts[0];
+      contractInstance = new web3.eth.Contract(window.abi, "0xE17F1b29A707a546E97B46154a6807A196e54509", {from: walletAddress});
 
       $("#make_bet_button").click(makeBet);
       $("#get_game_balance_button").click(getGameBalance);
+      $("#get_wallet_balance_button").click(getWalletBalance);
     });
 });
 
@@ -23,5 +26,11 @@ async function makeBet() {
 function getGameBalance() {
    contractInstance.methods.getGameBalance().call().then(function(res) {
     $("#game_balance").text(res + " wei");
+  });
+}
+
+function getWalletBalance() {
+  web3.eth.getBalance(walletAddress, function(err, result) {
+    $("#wallet_balance").text(result + " wei");
   });
 }
